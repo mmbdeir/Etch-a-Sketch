c3f7916d9e78536dd024db8f5295c0762a43ab0f
Mohamad <moeman.bdeir09@gmail.com>
2025-06-22 00:22:45 -0400

    Made it a always a perfect square, added a button to change pixel count.

--- script.js
+++ script.js
@@ -1,15 +1,36 @@
 const container = document.getElementById("container");
 
-for (let i = 0; i < 16 * 16; i++) {
-  const div = document.createElement("div");
-  div.setAttribute("id", i);
-  div.setAttribute("class", "boxes");
-  container.appendChild(div);
-  div.addEventListener(
-    "mouseover",
-    () => {
-      div.setAttribute("style", "background-color:white;");
-    },
-    false
-  );
+function CreateGrid(pixels = 16) {
+  for (let i = 0; i < pixels * pixels; i++) {
+    const div = document.createElement("div");
+    div.setAttribute("id", i);
+    div.setAttribute("class", "boxes");
+    container.appendChild(div);
+    div.addEventListener(
+      "mouseover",
+      () => {
+        div.setAttribute("style", "background-color:white;");
+      },
+      false
+    );
+  }
 }
+
+function DeleteGrid() {
+  container.innerHTML = "";
+}
+
+const button = document.getElementById("button");
+button.addEventListener("click", () => {
+  let pixels = prompt("How many pixels? (Make it less than 100)");
+  if (pixels < 100) {
+    let width = pixels * 30 + pixels * 4;
+    container.style.width = `${width}px`;
+    DeleteGrid();
+    CreateGrid((pixels = pixels));
+  } else {
+    alert("enter a valid number less than 100");
+  }
+});
+
+CreateGrid();
